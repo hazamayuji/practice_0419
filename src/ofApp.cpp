3af4ca0de9d809cfb8ea5a33f82629f28b915f4e
狭間祐至 <hazamayuji@mba.local>
2017-04-19 20:50:19 +0900

    教科書入出力入門の途中までです

---
+++ src/ofApp.cpp
@@ -0,0 +1,112 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(255,255,255);
+    ofSetColor(0, 0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    //ofDrawCircle(x_circle, y_circle, 50);
+    ofDrawCircle(ofGetWidth()/2, ofGetHeight()/2, size_circle);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if (key == 'a') {
+        size_circle = size_circle + 20;
+    }
+
+    if (key == 's') {
+        size_circle = size_circle - 20;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    if (key == 'q') {
+        size_circle = size_circle + 20;
+    }
+    
+    if (key == 'w') {
+        size_circle = size_circle - 20;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    /*
+    x_circle = x;
+    y_circle = y;
+    ofSetColor(ofRandom(255),ofRandom(255),ofRandom(255));
+     */
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    /*
+    if(button == 0){
+        ofSetColor(255, 0, 0);
+    }
+    
+    if(button == 1)
+    {
+        ofSetColor(0, 255, 0);
+    }
+    
+    if(button == 2)
+    {
+        ofSetColor(0, 0, 255);
+    }
+    
+    x_circle = x;
+    y_circle = y;
+    */
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    /*
+    x_circle = x;
+    y_circle = y;
+     */
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    /*
+    x_circle = ofGetWidth()/2;
+    y_circle = ofGetHeight()/2;
+    */
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
